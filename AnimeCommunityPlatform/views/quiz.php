5481479148998bca3a7ede2ec8ada4b01d5af894
Anne Alwala <anne.alwala@ashesi.edu.gh>
2024-04-12 20:07:02 +0000

    removing css file

--- AnimeCommunityPlatform/views/quiz.php
+++ AnimeCommunityPlatform/views/quiz.php
@@ -11,7 +11,7 @@ include '../settings/core.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz Game - API Project</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel = "stylesheet" href = "../assets/css/quiz.css">
+    
 
     <style>
 
